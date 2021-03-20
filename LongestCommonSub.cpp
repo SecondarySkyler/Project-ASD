c7f0b50e3a96aa7a1a5009dad135e0041ba0f38e
SecondarySkyler <skylerhunter6@gmail.com>
2021-03-20 17:44:15 +0100

    primo commit

---
+++ LongestCommonSub.cpp
@@ -0,0 +1,21 @@
+/**
+ * Si richiede di implementare un algoritmo
+ * che calcoli in tempo polinomiale una sotto-sequenza di lunghezza massima comune 
+ * a due stringhe fornite in input.
+ * 
+ * input: prati parti
+ * output: prti
+ * 
+ */
+#include <string>
+using std::string;
+
+int main() {
+
+}
+
+string LCS(string x, string y) {
+    if (x == "" || y == "") {
+        return "";
+    }
+}
