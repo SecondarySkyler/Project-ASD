1af336246318abc8df8486c6becdb99bc1f7492a
ManuelZuttion <manuel.zuttion@gmail.com>
2021-03-27 15:34:13 +0100

    To agne crote

---
+++ periodProject.hpp
@@ -0,0 +1,18 @@
+//
+// Created by Manuel Zuttion on 27/03/2021.
+//
+
+#ifndef PERIODPROJECT_CPP_PERIODPROJECT_HPP
+#define PERIODPROJECT_CPP_PERIODPROJECT_HPP
+
+
+
+int periodNaive (string s);
+
+int periodSmart (string s);
+
+double getResolution();
+
+string generator(int n);
+
+#endif //PERIODPROJECT_CPP_PERIODPROJECT_HPP
