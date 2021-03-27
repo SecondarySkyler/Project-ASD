02ba597ea60b7c39f5f0ffd08533e13498ad5521
SecondarySkyler <skylerhunter6@gmail.com>
2021-03-27 17:16:11 +0100

    added skeleton

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+
+
 #include "periodProject.hpp"
 
-constexpr int a{1000};
-constexpr int b{500000};
-constexpr double max_error{0.001};
+const int a = 1000;
+const double b = pow(500, 1/99);
+const double max_error = 0.001;
 const auto resolution{getResolution()};
 const int t_min{resolution * ((1 / max_error) + 1)};
+
+int main() {
+
+
+
+
+    void calcoloTempi() {
+        std::vector<double> naiveTime;
+        std::vector<double> smartTime;
+
+        // iter
+            // genero stringa s
+
+            // naive(s)
+            // smart(s)
+
+            //salvo tempi
+
+    }
+
+    
+
+}
