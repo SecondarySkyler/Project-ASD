a9a04828280c879e69d6db85285eb83dd2bf5f34
SecondarySkyler <skylerhunter6@gmail.com>
2021-03-27 15:45:01 +0100

    Added .hpp file

---
+++ periodProject.hpp
@@ -0,0 +1,18 @@
+//
+// Created by Manuel Zuttion on 27/03/2021.
+//
+
+#ifndef PERIODPROJECT_CPP_PERIODPROJECT_HPP
+#define PERIODPROJECT_CPP_PERIODPROJECT_HPP
+
+
+
+int periodNaive (std::string s);
+
+int periodSmart (std::string s);
+
+double getResolution();
+
+std::string generator(int n);
+
+#endif //PERIODPROJECT_CPP_PERIODPROJECT_HPP
