5faa55c93b43026878237d470d230f71b1c65a21
SecondarySkyler <skylerhunter6@gmail.com>
2021-05-22 09:40:24 +0200

    Eliminazione classe e implementazioni funzioni

--- Parte2/BST.cpp
+++ Parte2/BST.cpp
@@ -1,94 +1,69 @@
 #include <iostream>
 #include "BST.hpp"
-using namespace std;
 
 
-class BST {
-    private:
-        struct node {
-            int val;
-            node* left;
-            node* right;
-        };
-        
-        node* root;
+struct BST::node* root;
 
-        node* insert(int key, node* tree) {
-            if (tree == NULL)
-            {
-                tree = new node;
-                tree->val = key;
-                tree-> left = tree->right = NULL;
-            } 
-            else if(key < tree->val)
-                tree->left = insert(key, tree->left);
-            else if(key > tree->val)
-                tree->right = insert(key, tree->right);
-            
-            return tree;
-        }
-
-        node* find(int keyToFind, node* tree) {
-            if (tree == NULL)
-                return NULL;
-            else if (keyToFind = tree->val) 
-                return tree;
-            else if (keyToFind < tree->val) 
-                return find(keyToFind, tree->left);
-            else if (keyToFind > tree->val)
-                return find(keyToFind, tree->right);
-            else
-                return NULL;  
-        }
-
-        void inOrder(node* tree) {
-            if (tree != NULL)
-            {
-                inOrder(tree->left);
-                cout << tree->val << endl;
-                inOrder(tree->right);
-            }
-        }
-
-    public:
-        BST() {
-            root = NULL;
-        }
-        
-
-        void insert(int key) {
-            root = insert(key, root);
-        }
-
-        node* find(int key) {
-            return find(key, root);
-        }
-
-        void inOrder() {
-            inOrder(root);
-            cout << endl;
-        }
-};
-
-int main(int argc, char const *argv[])
-{
-    BST tree;
-    tree.insert(20);
-    tree.insert(23);
-    tree.insert(18);
-    tree.insert(26);
-    tree.insert(15);
-    tree.insert(17);
-    tree.insert(9);
-    tree.inOrder();
-    // if (tree.find(17) != NULL)
-    // {
-    //     cout << "dio" << endl;
-    // }
-    
-
-    return 0;
+BST::BST() {
+    root = NULL;
 }
+
+BST::node* BST::insertM(int key, node* tree) {
+    if (tree == NULL)
+        {
+            tree = new node;
+            tree->val = key;
+            tree-> left = tree->right = NULL;
+        } 
+        else if(key < tree->val)
+            tree->left = insertM(key, tree->left);
+        else if(key > tree->val)
+            tree->right = insertM(key, tree->right);
+        
+        return tree;
+}
+
+BST::node* BST::findM(int keyToFind, node* tree) {
+    if (tree == NULL)
+        return NULL;
+    else if (keyToFind = tree->val) 
+        return tree;
+    else if (keyToFind < tree->val) 
+        return findM(keyToFind, tree->left);
+    else if (keyToFind > tree->val)
+        return findM(keyToFind, tree->right);
+    else
+        return NULL;
+}
+
+void BST::inOrderM(node *tree) {
+    if (tree != NULL)
+    {
+        inOrderM(tree->left);
+        std::cout << tree->val << std::endl;
+        inOrderM(tree->right);
+    }
+}
+
+/**
+ * 
+ * IMPLEMENTAZIONE FUNZIONI PUBBLICHE
+ * 
+ */
+
+void BST::insert(int key) {
+    root = insertM(key, root);
+}
+
+BST::node* BST::find(int keyToFind) {
+    return findM(keyToFind, root);
+}
+
+void BST::inOrder() {
+    inOrderM(root);
+}
+
+
 
 
 
