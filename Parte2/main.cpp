6e94212f49558cfd34f37a62f562da40160a6410
ManuelZuttion <manuel.zuttion@gmail.com>
2021-05-23 16:08:56 +0200

    Added ammortized time measure

--- Parte2/main.cpp
+++ Parte2/main.cpp
@@ -13,23 +13,23 @@ const double max_error = 0.01;
 const auto resolution = getResolution();
 const double t_min = resolution * ((1 / max_error) + 1);
 
-void calcoloTempi(BST);
+void calcoloTempi(AVL);
 
 // compile with g++ -g AVL.cpp BST.cpp utilities.cpp main.cpp -o main
 int main(int argc, char **argv) {
-    BST tree;
+    AVL tree;
     calcoloTempi(tree);
     std::cout << std::endl;
 }
 
-void calcoloTempi(BST tree) {
+void calcoloTempi(AVL tree) {
 
-    std::ofstream csv(/*treeType + */"BST.csv"); // csv per la scrittura
+    std::ofstream csv(/*treeType + */"AVL.csv"); // csv per la scrittura
 
     // Calcolo tempi Naive
     for (int j = 0; j < 100; j++)
     {
-        int time;
+        double time;
         int n = a * std::pow(1000, ((j + 0.0)/99)); // calcolo numero elementi
         int iterCount = 0;
         int key = 0;
@@ -42,13 +42,12 @@ void calcoloTempi(BST tree) {
                 if (iterCount == 0) key = std::rand();
                 if (tree.find(key) == NULL) tree.insert(key);
             }
-            
 
             end = std::chrono::steady_clock::now();
             iterCount++;
         } while (std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() < t_min);
         
-        time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / iterCount;
+        time = ((std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() + 0.0) / iterCount)/n;
         csv << std::to_string(n) << ", " << std::to_string(time) + "\n"; // scrittura su csv 
         std::cout << /*treeType +*/ " Iterazione : " + std::to_string(j) << std::endl;
     }
