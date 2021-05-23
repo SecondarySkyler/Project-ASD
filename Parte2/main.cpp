9369b5130080867f775effdd2fc0559a1248b71a
ManuelZuttion <manuel.zuttion@gmail.com>
2021-05-23 15:41:27 +0200

    Added function to measure in main

--- Parte2/main.cpp
+++ Parte2/main.cpp
@@ -3,47 +3,54 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "utilities.hpp"
 #include "BST.hpp"
 #include "AVL.hpp"
 
-
+const int a = 1000;
 const double max_error = 0.01;
 const auto resolution = getResolution();
 const double t_min = resolution * ((1 / max_error) + 1);
 
+void calcoloTempi(BST);
+
 // compile with g++ -g AVL.cpp BST.cpp utilities.cpp main.cpp -o main
 int main(int argc, char **argv) {
-    AVL tree;
-    tree.insert(20);
-    tree.insert(12);
-    tree.insert(100);
-    tree.inOrder();
-    // delete tree;
+    BST tree;
+    calcoloTempi(tree);
     std::cout << std::endl;
 }
 
-void calcoloTempi(/*treeType*/) {
+void calcoloTempi(BST tree) {
 
-    std::ofstream avl(/*treeType + */".csv"); // csv per la scrittura
+    std::ofstream csv(/*treeType + */".csv"); // csv per la scrittura
 
     // Calcolo tempi Naive
     for (int j = 0; j < 100; j++)
     {
-        int* k; /* !!! TO ALLOCATE AND DEFINE !!! */
+        int time;
+        int n = a * std::pow(100, ((j + 0.0)/99)); // calcolo numero elementi
         int iterCount = 0;
+        int key = 0;
         std::chrono::steady_clock::time_point end;
         std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
         do
         {
-            /* !!!  INSERT HERE FUNCTION TO MEASURE !!! */
+            for (int i = 0; i < n; i++)
+            {
+                if (iterCount == 0) key = std::rand();
+                if (tree.find(key) == NULL) tree.insert(key);
+            }
+            
+
             end = std::chrono::steady_clock::now();
             iterCount++;
         } while (std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() < t_min);
         
-        int t = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / iterCount;
-        avl << k[j] << ", " << t << "\n"; // scrittura su csv 
-        std::cout << /*treeType +*/ " Iterazione : " +  std::to_string(j) << std::endl;
+        time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / iterCount;
+        csv << n << ", " << time + "\n"; // scrittura su csv 
+        std::cout << /*treeType +*/ " Iterazione : " + std::to_string(j) << std::endl;
     }
-    avl.close();
+    csv.close();
 }
