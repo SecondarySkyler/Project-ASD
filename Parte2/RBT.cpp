29241efcf0da2365c36d605315eb71fc20ada03f
SecondarySkyler <skylerhunter6@gmail.com>
2021-05-26 11:14:24 +0200

    Added find function and partial implementation of insert

--- Parte2/RBT.cpp
+++ Parte2/RBT.cpp
@@ -5,7 +5,29 @@
 /**
  * Implementazione funzioni private
  */
+RBT::node* RBT::insert(int key, node* tree) {
+    if (tree == NULL)
+    {
+        tree = new node;
+        tree->val = key;
+        tree->left = tree->right = NULL;
+        tree->colore = Color::BLACK;
+    }
+    // more to add
+}
 
+RBT::node* RBT::find(int keyToFind, node* tree) {
+    if (tree == NULL)
+        return NULL;
+    else if (keyToFind = tree->val) 
+        return tree;
+    else if (keyToFind < tree->val) 
+        return find(keyToFind, tree->left);
+    else if (keyToFind > tree->val)
+        return find(keyToFind, tree->right);
+    else
+        return NULL;
+}
 
 /**
  * Implementazione funzioni pubbliche
