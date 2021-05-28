add90bc848869c0a07d4581d6bb3c4965ea56a3c
SecondarySkyler <skylerhunter6@gmail.com>
2021-05-28 18:16:20 +0200

    Added implementation for balancing functions

--- Parte2/RBT.cpp
+++ Parte2/RBT.cpp
@@ -9,12 +9,84 @@ RBT::node* RBT::parent(node* tree) {
     return tree->parent;
 }
 
+void RBT::balance(node* tree) {
+
+    if (tree->isLeft() & tree->parent->isLeft()) // lo zio se esiste e' right
+    {
+        if (tree->parent->parent->right = NULL)
+        {
+            rightRotate(tree->parent->parent);
+            tree->parent->colore = Color::BLACK;
+            tree->parent->right->colore = Color::RED;
+            return;
+        }
+        
+        // caso 1 sx 
+        if (tree->parent->parent->right->colore == Color::BLACK)    
+        {
+            rightRotate(tree->parent->parent);
+            tree->parent->colore = Color::BLACK;
+            tree->parent->right->colore = Color::RED;
+            return;
+        }
+        else // zio rosso
+        {
+            tree->parent->colore = Color::BLACK;
+            tree->parent->parent->colore = Color::RED;
+            tree->parent->parent->right->colore = Color::BLACK;
+            balance(tree->parent->parent);
+            return;
+        }
+        
+    } 
+    else if ((!tree->isLeft()) & (!tree->parent->isLeft()))
+    {
+        if (tree->parent->parent->left = NULL)
+        {
+            rightRotate(tree->parent->parent);
+            tree->parent->colore = Color::BLACK;
+            tree->parent->left->colore = Color::RED;
+            return;
+        }
+        // caso 1 dx
+        if (tree->parent->parent->left->colore == Color::BLACK)    
+        {
+            leftRotate(tree->parent->parent);
+            tree->parent->colore = Color::BLACK;
+            tree->parent->left->colore = Color::RED;
+            return;
+        }
+        else // zio rosso
+        {
+            tree->parent->colore = Color::BLACK;
+            tree->parent->parent->colore = Color::RED;
+            tree->parent->parent->left->colore = Color::BLACK;
+            balance(tree->parent->parent);
+            return;
+        }
+
+    }
+    else if (tree->isLeft() && !tree->parent->isLeft()) // caso 2 sx
+    {
+        leftRotate(tree->parent);
+        balance(tree->left);
+        return;
+    }
+    else if (!tree->isLeft() && tree->parent->isLeft()) // caso 2 dx
+    {
+        rightRotate(tree->parent);
+        balance(tree->right);
+        return;
+    }
+  
+}
+
 RBT::node* RBT::insert(int key, node* tree) {
     if (tree == NULL)
     {
         tree = new node;
         tree->val = key;
-        tree->left = tree->right = NULL;
+        tree->parent = tree->left = tree->right = nullptr;
         tree->colore = Color::RED;
     }
     // more to add
@@ -24,9 +96,42 @@ RBT::node* RBT::insert(int key, node* tree) {
     }
 
     if (key < tree->val)
-        tree->left = insert(key, tree->left);
+    {
+        if (tree->left == nullptr)
+        {
+            tree->left = new node;
+            tree->left->val = key;
+            tree->left->parent = tree;
+            tree->left->left = tree->left->right = NULL;
+            tree->left->colore = Color::RED;
+        }
+        else
+        {
+            tree->left = insert(key, tree->left);
+        }
+    }
     else if (key > tree->val)
-        tree->right = insert(key, tree->right);
+    {
+        if (tree->right == nullptr)
+        {
+            tree->right = new node;
+            tree->right->val = key;
+            tree->right->parent = tree;
+            tree->right->left = tree->right->right = NULL;
+            tree->right->colore = Color::RED;
+        }
+        else
+        {
+            tree->right = insert(key, tree->right);
+        }
+    }
+
+    // check the need to restablish the invariant
+    if (tree->parent != NULL && tree->colore == Color::RED && tree->parent->colore == Color::RED)
+    {
+        balance(tree);
+    }
+    
     
     return tree;
 }
@@ -44,6 +149,46 @@ RBT::node* RBT::find(int keyToFind, node* tree) {
         return NULL;
 }
 
+void RBT::leftRotate(node*& tree) {
+    node *right_child = tree->right;
+    tree->right = right_child->left;
+
+    if (tree->right != nullptr)
+        tree->right->parent = tree;
+
+    right_child->parent = tree->parent;
+
+    if (tree->parent == nullptr)
+        root = right_child;
+    else if (tree == tree->parent->left)
+        tree->parent->left = right_child;
+    else
+        tree->parent->right = right_child;
+
+    right_child->left = tree;
+    tree->parent = right_child;
+}
+
+
+void RBT::rightRotate(node*& tree) {
+    node *left_child = tree->left;
+    tree->left = left_child->right;
+
+    if (tree->left != nullptr)
+        tree->left->parent = tree;
+
+    left_child->parent = tree->parent;
+
+    if (tree->parent == nullptr)
+        root = left_child;
+    else if (tree == tree->parent->left)
+        tree->parent->left = left_child;
+    else
+        tree->parent->right = left_child;
+
+    left_child->right = tree;
+    tree->parent = left_child;
+}
 
 void RBT::inOrder(node *tree) {
     if (tree != NULL)
@@ -52,6 +197,16 @@ void RBT::inOrder(node *tree) {
         std::cout << tree->val << std::endl;
         inOrder(tree->right);
     }
+}
+
+int RBT::heightChecker(node* n) {
+    int count = 1;
+    while (n->right != nullptr)
+    {
+        n = n->right;
+        count++;
+    }
+    return count;
 }
 
 /**
@@ -74,12 +229,17 @@ void RBT::inOrder() {
     inOrder(root);
 }
 
+int RBT::heightChecker() {
+    return heightChecker(root);
+}
+
 int main(int argc, char const *argv[])
 {
     RBT tree;
     tree.insert(5);
-    tree.insert(10);
-    tree.insert(3);
-    tree.inOrder();
+    tree.insert(24);
+    tree.insert(34);
+    // tree.inOrder();
+
     return 0;
 }
