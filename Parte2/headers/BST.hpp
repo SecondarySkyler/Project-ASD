90f5d1620b5c45a76645c20e1ffaa6f58f2fb3c9
SecondarySkyler <skylerhunter6@gmail.com>
2021-06-19 14:00:22 +0200

    Added prototypes  of polishPrint

--- Parte2/headers/BST.hpp
+++ Parte2/headers/BST.hpp
@@ -1,6 +1,6 @@
 #ifndef _BST_H
 #define _BST_H
-
+#include <map>
 
 
 class BST {
@@ -18,6 +18,8 @@ class BST {
         void insert(int);
         node* find(int);
         void inOrder();
+        void polishPrint(std::map<int, std::string>);
+
         
 
     private:
@@ -25,6 +27,7 @@ class BST {
         node* insertM(int, node*);
         node* findM(int, node*);
         void inOrderM(node*);
+        void polishPrint(std::map<int, std::string>, node*);
 };
 
 
