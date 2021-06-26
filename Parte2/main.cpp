cf68f2cc8f7212ce75d4f88d1e59b22b3884ea52
SecondarySkyler <skylerhunter6@gmail.com>
2021-06-26 10:00:45 +0200

    Implementation for calculating execution times and MSE

--- Parte2/main.cpp
+++ Parte2/main.cpp
@@ -2,77 +2,85 @@
 #include <chrono>
 #include <iostream>
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
 #include <math.h>
+#include <vector>
+#include <typeinfo>
 #include "headers/utilities.hpp"
 #include "headers/RBT.hpp"
+#include "headers/BST.hpp"
+#include "headers/AVL.hpp"
 
 const int a = 1000;
 const double max_error = 0.01;
 const auto resolution = getResolution();
 const double t_min = resolution * ((1 / max_error) + 1);
 
-void calcoloTempi(void);
-void calcVar(void);
+// template<class T>
+// std::vector<std::pair<double, double>> calcoloTempi(T tree) {
 
-// compile with g++ -g RBT.cpp utilities.cpp main.cpp -o main
-int main(int argc, char **argv) {
-    srand(time(NULL));
-    calcoloTempi();
-    // calcVar();
-    std::cout << std::endl;
-}
+//     std::vector<std::pair<double, double>> results;
+//     std::ofstream csv( ((std::string) typeid(tree).name()).substr(1, 3) + ".csv"); // csv per la scrittura
+    
+//     for (int j = 0; j < 100; j++)
+//     {
+//         double tempoAmmortizzato;
+//         int n = a * std::pow(1000, ((j + 0.0)/99)); // calcolo numero elementi
+//         int iterCount = 0;
+//         int key = 0;
+//         std::chrono::steady_clock::time_point end;
+//         std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+//         // int insCount;
+//         do
+//         {
+//             T tree;
+//             // insCount = 0;
+//             for (int i = 0; i < n; i++)
+//             {
+//                 key = std::rand();
 
-void calcoloTempi() {
+//                 if (tree.find(key) == nullptr) {
+//                     tree.insert(key);
+//                     // insCount++;
+//                 }
+//             }
+//             end = std::chrono::steady_clock::now();
+//             iterCount++;
+//         } while (std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() < t_min);
 
-    std::ofstream csv(/*treeType + */"RBT.csv"); // csv per la scrittura
-
-    for (int j = 0; j < 100; j++)
-    {
-        double time;
-        int n = a * std::pow(1000, ((j + 0.0)/99)); // calcolo numero elementi
-        int iterCount = 0;
-        int key = 0;
-        std::chrono::steady_clock::time_point end;
-        std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
-        int insCount;
-        do
-        {
-            RBT tree;
-            insCount = 0;
-            for (int i = 0; i < n; i++)
-            {
-                key = (std::rand() * std::rand() % n);
-
-                if (tree.find(key) == nullptr) {
-                    tree.insert(key);
-                    insCount++;
-                }
-            }
-            end = std::chrono::steady_clock::now();
-            iterCount++;
-        } while (std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() < t_min);
+//         double var = calcVar(n);
         
-        std::cout << n << "  Insertions: " + std::to_string(insCount) << std::endl;
-        time = ((std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() + 0.0) / iterCount)/n;
-        csv << std::to_string(n) << ", " << std::to_string(time) + "\n"; // scrittura su csv 
-        std::cout << /*treeType +*/ " Iterazione : " + std::to_string(j) << std::endl;
-    }
-    std::cout << "Terminato!" << std::endl;
-    csv.close();
-}
+//         // std::cout << n << "  Insertions: " + std::to_string(insCount) << std::endl;
+//         tempoAmmortizzato = ((std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() + 0.0) / iterCount) / n;
+//         results.push_back(std::make_pair(tempoAmmortizzato, var));
+//         csv << std::to_string(n) << ", " << std::to_string(tempoAmmortizzato) + "\n"; // scrittura su csv 
+//         std::cout << /*treeType +*/ " Iterazione : " + std::to_string(j) << std::endl;
+//     }
+//     std::cout << "Terminato!" << std::endl;
+//     csv.close();
+//     return results;
+// }
 
-void calcVar() {
 
-    std::ofstream csv(/*treeType + */"RBT_var.csv"); // csv per la scrittura
+/*
+* CALCOLO VARIANZA
+*/
+template<class T>
+std::vector<std::pair<double, double>> calcVar(T tree) {
 
+    std::vector<std::pair<double, double>> result;
+    
     for (int j = 0; j < 100; j++)
     {
+        double var;
+        double averageTime;
+        double arrayOfTimes[20];
+        int n;
         for(int iter = 0; iter < 20; iter++)
         {
-            RBT tree;
-            double time;
-            int n = a * std::pow(1000, ((j + 0.0)/99)); // calcolo numero elementi
+            double tempoAmmortizzato;
+            n = a * std::pow(1000, ((j + 0.0)/99)); // calcolo numero elementi
 
             int iterCount = 0;
             int key = 0;
@@ -80,9 +88,10 @@ void calcVar() {
             std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
             do
             {
+                T tree;
                 for (int i = 0; i < n; i++)
                 {
-                    if (iterCount == 0) key = (std::rand() * std::rand() % n);
+                    if (iterCount == 0) key = (std::rand());
                     if (tree.find(key) == nullptr) tree.insert(key);
                 }
 
@@ -90,11 +99,47 @@ void calcVar() {
                 iterCount++;
             } while (std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() < t_min);
             
-            time = ((std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() + 0.0) / iterCount)/n;
-            csv << std::to_string(n) << ", " << std::to_string(time) + "\n"; // scrittura su csv 
+            tempoAmmortizzato = ((std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() + 0.0) / iterCount)/n;
+            averageTime += tempoAmmortizzato;
+            arrayOfTimes[iter] = tempoAmmortizzato;
+
             std::cout << /*treeType +*/ " Iterazione : " + std::to_string(j) + " SubIter: " + std::to_string(iter+1) << std::endl;
         }
+        averageTime /= 20;
+        for (int i = 0; i < 20; i++)
+        {
+            var += (arrayOfTimes[i] - averageTime) * (arrayOfTimes[i] - averageTime);
+        }
+        var /= 20;
+        result.push_back(std::make_pair(averageTime, var));
+        
     }
     std::cout << "Terminato!" << std::endl;
-    csv.close();
+    return result;
+}
+
+void printRecord(std::vector<std::pair<double, double>> bst, std::vector<std::pair<double, double>> rbt, std::vector<std::pair<double, double>> avl) {
+    std::ofstream csv("Records.csv"); // csv per la scrittura
+    for (int i = 0; i < bst.size(); i++)    
+    {   
+        int n = a * std::pow(1000, ((i + 0.0)/99));
+        csv << std::to_string(n) <<  "BST, " << bst[i].first << ", " << bst[i].second << ", "
+                                    << "RBT, " << rbt[i].first << ", " << rbt[i].second << ", "
+                                    << "AVL, " << avl[i].first << ", " << avl[i].second << std::endl;
+    }
+    
+}
+
+// compile with g++ -g RBT.cpp utilities.cpp main.cpp -o main
+int main(int argc, char **argv) {
+    srand(time(NULL));
+    BST bst_tree;
+    AVL avl_tree;
+    RBT rbt_tree;
+    
+    std::vector<std::pair<double, double>> tempiBst = calcVar(bst_tree);
+    std::vector<std::pair<double, double>> tempiAvl = calcVar(avl_tree);
+    std::vector<std::pair<double, double>> tempiRbt = calcVar(rbt_tree);
+    printRecord(tempiBst, tempiRbt, tempiAvl);
+    // calcVar();
 }
