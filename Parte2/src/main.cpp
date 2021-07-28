9ea14f42c202351b5b95545bd0015cd2773c9f9f
SecondarySkyler <skylerhunter6@gmail.com>
2021-07-28 17:09:43 +0200

    Added comments, fixed problem causing seg fault

--- Parte2/src/main.cpp
+++ Parte2/src/main.cpp
@@ -18,6 +18,10 @@ const double max_error = 0.01;
 const auto resolution = getResolution();
 const double t_min = resolution * ((1 / max_error) + 1);
 
+/**
+ * Metodo per popolare un vettore con chiavi generate in modo pseudo casuale
+ * 
+ * **/
 void fillVectorWithNodes(std::vector<double> &keysVector, int n) {
     for (int h = 0; h < n; h++)
     {
@@ -25,13 +29,23 @@ void fillVectorWithNodes(std::vector<double> &keysVector, int n) {
     }
 }
 
+/**
+ * Metodo per popolare un vettore con chiavi generate in modo deterministico
+ * Usato per la verifica del caso pessimo
+ * **/
 void fillVectorWorstCase(std::vector<double> &vector, int n) {
-    for (int h = 0; h < n; h++)
+    for (int h = 1; h <= n; h++)
     {
         vector.push_back(h);
     }    
 }
 
+/**
+ * Metodo per il calcolo della varianza
+ * il parametro n è la dimensione del set di nodi da generare
+ * la varianza è ottenuta iterando per 20 volte le operazioni di inserimento/ricerca
+ * sul set di n nodi, a ogn iterazione il set viene rigenerato  
+ **/
 template<class T>
 double calcoloVarianza(int n) {
 
@@ -39,17 +53,12 @@ double calcoloVarianza(int n) {
     double averageTime {0.0};
     std::array<double, 20> arrayOfTimes;
 
-    // for (int j = 0; j < 100; j++)
-    // {
-        // int n = a * std::pow(1000, ((j + 0.0)/99));
-
         for (int i = 0; i < 20; i++)
         {
             double executionTime;
             std::vector<double> keys;  
             keys.reserve(n);
             fillVectorWithNodes(keys, n); // popolo il vettore con n nodi
-
 
             int iterationsCounter = 0;
             std::chrono::steady_clock::time_point end;
@@ -82,62 +91,69 @@ double calcoloVarianza(int n) {
             varianza += pow((time - averageTime), 2);
         }
         varianza /= 20;
-    // }
     
     return varianza;  
 }
 
 template<class T>
-std::pair<double, double> calcoloTempi(std::vector<double> &vectorOfKeys, int n) {
+double calcoloTempi(std::vector<double> &vectorOfKeys, int n) {
 
-    std::pair<double, double> results;
-    
-    // for (int j = 0; j < 100; j++)
-    // {
-        double tempoAmmortizzato;
-        // int n = a * std::pow(1000, ((j + 0.0)/99)); // calcolo numero elementi
-        int iterCount = 0;
+    double tempoAmmortizzato;
+    int iterCount = 0;
 
-        std::chrono::steady_clock::time_point end;
-        std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+    std::chrono::steady_clock::time_point end;
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
 
-        do
+    do
+    {
+        T timeTree;
+
+        for (const auto &key : vectorOfKeys)
         {
-            T timeTree;
-
-            for (const auto &key : vectorOfKeys)
-            {
-                if (timeTree.find(key) == nullptr) {
-                    timeTree.insert(key);
-                }
+            if (timeTree.find(key) == nullptr) {
+                timeTree.insert(key);
             }
-            end = std::chrono::steady_clock::now();
-            iterCount++;
+        }
+        end = std::chrono::steady_clock::now();
+        iterCount++;
 
-        } while (std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() < t_min);
+    } while (std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() < t_min);
 
-        tempoAmmortizzato = ((std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() + 0.0) / iterCount) / n;
-        double varianza = calcoloVarianza<T>(n);
+    tempoAmmortizzato = ((std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() + 0.0) / iterCount) / n;
 
-        results = std::make_pair(tempoAmmortizzato, varianza); // sarebbe da eseguire un make_pair 
-
-    // }
-    
-    return results;
+    return tempoAmmortizzato;
 }
 
 
 
 void printRecordCsv(std::vector<std::pair<double, double>> &bst, std::vector<std::pair<double, double>> &rbt, std::vector<std::pair<double, double>> &avl) {
-    std::ofstream csv("Records_DiffStd.csv"); // csv per la scrittura
+    std::ofstream csv("Records_tempo_varianza.csv"); // csv per la scrittura
     for (int i = 0; i < bst.size(); i++)    
     {   
         int n = a * std::pow(1000, ((i + 0.0)/99));
         csv << std::to_string(n) <<  ", " << "BST, " << bst[i].first << ", " << bst[i].second << ", " 
                                           << "RBT, " << rbt[i].first << ", " << rbt[i].second << ", "
                                           << "AVL, " << avl[i].first << ", " << avl[i].second << ", " << "\n";
+    } 
+    csv.close();  
+}
+
+void printWorstCaseCsv(std::vector<double> &bst, std::vector<double> &rbt, std::vector<double> &avl) {
+    std::ofstream csv("Records_tempo_worst_bst.csv"); // csv per la scrittura
+    for (int i = 0; i < bst.size(); i++)
+    {
+        int n = a * std::pow(1000, ((i + 0.0)/99));
+        csv << std::to_string(n) << ", " << "BST, " << bst[i] << "\n";
     }
+    csv.close();
     
+    std::ofstream csv2("Records_tempo_worst_rbt&avl.csv");
+    for (int i = 0; i < rbt.size(); i++)
+    {
+        int n = a * std::pow(1000, ((i + 0.0)/99));
+        csv2 << std::to_string(n) << ", " << "RBT, " << rbt[i] << ", " << "AVL, " << avl[i] << "\n"; 
+    }
+    csv2.close();
 }
 
 
@@ -162,24 +178,40 @@ int main(int argc, char **argv) {
         keys.reserve(n);
         fillVectorWithNodes(keys, n); // popolo il vettore dei nodi di modo che le n operazioni avvengano sullo stesso set per i 3 alberi
 
-        auto bst_result = calcoloTempi<BST>(keys, n);
-        auto avl_result = calcoloTempi<AVL>(keys, n);
-        auto rbt_result = calcoloTempi<RBT>(keys, n);
+        std::cout << std::to_string(i) << std::endl;
 
-        tempi_varianza_bst.push_back(bst_result);
-        tempi_varianza_avl.push_back(avl_result);
-        tempi_varianza_rbt.push_back(rbt_result);   
+        tempi_varianza_bst.push_back(std::make_pair(calcoloTempi<BST>(keys, n), calcoloVarianza<BST>(n)));
+        tempi_varianza_avl.push_back(std::make_pair(calcoloTempi<AVL>(keys, n), calcoloVarianza<AVL>(n)));
+        tempi_varianza_rbt.push_back(std::make_pair(calcoloTempi<RBT>(keys, n), calcoloVarianza<RBT>(n)));   
+
     }
 
-    // for (int j = 0; j < tempi_varianza_bst.size(); j++)
-    // {
-    //     std::cout << "BST, " << tempi_varianza_bst[j].first << " " << tempi_varianza_bst[j].second << " " 
-    //               << "AVL, " << tempi_varianza_avl[j].first << " " << tempi_varianza_avl[j].second << " "
-    //               << "RBT, " << tempi_varianza_rbt[j].first << " " << tempi_varianza_rbt[j].second << "\n"; 
-    // }
-    
+    std::vector<double> tempoWorstCaseBst;
+    tempoWorstCaseBst.reserve(100);
+
+    std::vector<double> tempoWorstCaseAvl;
+    tempoWorstCaseAvl.reserve(100);
+
+    std::vector<double> tempoWorstCaseRbt;
+    tempoWorstCaseRbt.reserve(100);
+
+    for (int z = 0; z < 100; z++)
+    {
+        std::cout << "Worst case bst/avl/rbt iteration " + std::to_string(z) << "\n";
+        int n = a * std::pow(1000, ((z + 0.0) / 99));
+        std::vector<double> keys;
+        keys.reserve(100);
+        fillVectorWorstCase(keys, n);
+
+        if(n < 200000) {
+            tempoWorstCaseBst.push_back(calcoloTempi<BST>(keys, n));   
+        }
+
+        tempoWorstCaseAvl.push_back(calcoloTempi<AVL>(keys, n));
+        tempoWorstCaseRbt.push_back(calcoloTempi<RBT>(keys, n));
+    }
     
     printRecordCsv(tempi_varianza_bst, tempi_varianza_rbt, tempi_varianza_avl);
+    printWorstCaseCsv(tempoWorstCaseBst, tempoWorstCaseRbt, tempoWorstCaseAvl);
 
-    
 }
